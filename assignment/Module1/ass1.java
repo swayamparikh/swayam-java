e2dbe9627c2fb2ac5122d517a2e95aea4a618ff4
swayamparikh <157677344+swayamparikh@users.noreply.github.com>
2024-02-16 17:45:11 +0530

    Update ass1.java

--- assignment/Module1/ass1.java
+++ assignment/Module1/ass1.java
@@ -1,1 +1,32 @@
+package com.ass;
+
+import java.util.Scanner;
+
+public class ass1
+{
+	public static void main(String[] args) 
 	{
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter The Number A:- ");
+		int a=sc.nextInt();
+		System.out.println("Enter The Number B:- ");
+		int b=sc.nextInt();
+		System.out.println("Enter The Number C:- ");
+		int c=sc.nextInt();
+		
+		
+		if(a>=b && a>=c)
+		{
+			System.out.println("A is Max...");
+		}
+		else if(b>=a && b>=c)
+		{
+			System.out.println("B is Max...");
+		}
+		else
+		{
+			System.out.println("C is Max..");
+		}
+			
+	}
+}
